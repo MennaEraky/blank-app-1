db98856b0699996dfae0bbe8f1b7445355791524
MennaEraky <mennahesham62@yahoo.com>
2024-08-27 01:33:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ feature_5 = st.sidebar.number_input("DiabetesPedigreeFunction")
 feature_6 = st.sidebar.number_input("Age")
 
 # Prepare the input data
-input_data = np.array([[feature_1, feature_2,feature_3,feature_4,feature_5,feature_6]])
+input_data = np.array([[feature_1, feature_2,feature_3,feature_4,feature_5]])
 
 # Check if the input data has missing values
 if np.any(np.isnan(input_data)):
