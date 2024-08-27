a616e04cb9144f6b418f2026344e506b3235b6d2
MennaEraky <mennahesham62@yahoo.com>
2024-08-27 14:38:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,38 @@
+
 import streamlit as st
 import pickle
 import numpy as np
 
-# Load the model
-try:
-    with open('logistic.pkl', 'rb') as file:
-        model = pickle.load(file)
-except Exception as e:
-    st.error(f"Error loading model: {e}")
-    st.stop()
+# load pkl file
+with open('tree_clf.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+
+# Check the number of features the model expects
+n_features = model.n_features_in_
 
 # Streamlit App
-st.title("Decision Tree Prediction")
+st.title("Linear Regression Prediction")
 
 # Input features
 st.sidebar.header("Input Features")
-feature_1 = st.sidebar.number_input("Pregnancies",value=10.0)
-feature_2 = st.sidebar.number_input("Glucose",value=10.0)
-feature_3 = st.sidebar.number_input("Insulin")
-feature_4 = st.sidebar.number_input("BMI")
-feature_5 = st.sidebar.number_input("DiabetesPedigreeFunction")
-feature_6 = st.sidebar.number_input("Age")
+feature_1 = st.sidebar.number_input("Feature 1", value=1.0)
+feature_2 = st.sidebar.number_input("Feature 2", value=2.0)
+feature_3 = st.sidebar.number_input("Feature 3", value=3.0)
+feature_4 = st.sidebar.number_input("Feature 4", value=3.0)
+feature_5 = st.sidebar.number_input("Feature 5", value=3.0)
+feature_6 = st.sidebar.number_input("Feature 6", value=3.0)
 
 # Prepare the input data
-input_data = np.array([[feature_1, feature_2,feature_3,feature_4,feature_5,feature_6]])
-# Test input
-#input_data = np.array([[10.0, 10.0, 50.0, 30.0, 0.5, 25.0]])  # Adjust values accordingly
+input_data = np.array([[feature_1, feature_2, feature_3,feature_4,feature_5,feature_6]])
 
-# # Make prediction
-# prediction = model.predict(input_data)
-# print("Predicted Output:", prediction[0])
-
-#Check if the input data has missing values
-if np.any(np.isnan(input_data)):
-    st.error("Input data contains missing values. Please provide valid input.")
+# Check if the input data has the correct number of features
+if input_data.shape[1] != n_features:
+    st.error(f"The model expects {n_features} features, but received {input_data.shape[1]} features.")
 else:
-
     # Make prediction
     prediction = model.predict(input_data)
+
+    # Display the prediction
     st.write("### Predicted Output")
     st.write(prediction[0])
-
